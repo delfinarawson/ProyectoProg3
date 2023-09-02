8bbb56d2d168cb64e5fe6416ca88f5c1f101e210
justinaalric <jalricferre@udesa.edu.ar>
2023-09-02 12:34:08 -0300

    header casi listo, falta la parte de rect con etiquetas link

--- app-react/src/App.js
+++ app-react/src/App.js
@@ -1,8 +1,10 @@
 import React from "react";
 import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
 function App() {
   return (
       <body>
+        <Header/>
     <h1>My App in React</h1>
     <main>
     </main>
--- app-react/src/components/Header/Header.js
+++ app-react/src/components/Header/Header.js
@@ -1,7 +1,14 @@
 import React from "react";
 function Header(props){
     return(
-        <img></img>        
+        <nav>
+            <img alt="logo"></img>
+            <ul className="main-nav">
+            <li>Home</li>
+            <li>Favoritos</li>
+            <li>Ver todas</li>
+        </ul>
+    </nav>      
     )
 }
 export default Header;
