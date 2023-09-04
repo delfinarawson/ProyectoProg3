f8973089fc99c568aeb7c396455c8d46ab6f6435
delfinarawson <drawson@udesa.edu.ar>
2023-09-04 16:53:42 -0300

    css

--- app-react/src/components/Header/Header.js
+++ app-react/src/components/Header/Header.js
@@ -2,15 +2,15 @@ import React from "react";
 import {Link} from "react-router-dom";
 function Header(props){
     return(
-    <header>
+    <header className="header">
         <img src="../public/logo" className="logo" alt="logo"></img>
         <nav className="inicio">
-            <ul className="main-nav">
+            <ul className="inicio">
             <li> <Link to ="/">Home</Link></li>
             <li> <Link to ="/favoritos">Favoritos</Link></li>
             <li> <Link to ="/populares">Populares</Link></li>
             <li> <Link to ="/upcoming">Upcoming</Link></li>
-        </ul>
+            </ul>
         </nav> 
     </header>     
     )
