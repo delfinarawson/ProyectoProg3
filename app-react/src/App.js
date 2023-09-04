bb70196952a8a474ca51e2e9ef49c3cb0632cf79
delfinarawson <drawson@udesa.edu.ar>
2023-09-04 15:05:11 -0300

    screens

--- app-react/src/App.js
+++ app-react/src/App.js
@@ -1,13 +1,19 @@
 import React from "react";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
-import {Route} from "react-router-dom";
+import {BrowserRouter, Link, Route, Switch} from 'react-router-dom';
+
+
 function App() {
   return (
       <body>
         <Header/>
     <h1>My App in React</h1>
     <main>
+      <Switch>
+        <Route path="/" component={Home} />
+        <Route path="/favoritos" component={Favoritos} />
+      </Switch>
     </main>
   <Footer/>
     </body>
