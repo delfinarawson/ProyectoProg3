0b9b3cd6a4f0177832c9ae130c38f2ac40798a3d
justinaalric <jalricferre@udesa.edu.ar>
2023-09-04 17:54:01 -0300

    arreglando home

--- app-react/src/screens/Home/Home.js
+++ app-react/src/screens/Home/Home.js
@@ -1,14 +1,18 @@
-import React from "react";
+import React, { Component } from "react";
 import Buscador from "../../components/Buscador/Buscador";
 import Pelis from "../../components/Pelis/Pelis"
 import PelisUp from "../../components/PelisUp/PelisUp.js"
 
-function Home(props){
-    this.setState({
-        populares:[],
-        upcoming: []
-    })
-    componentDidMount();{
+class Home extends Component{
+    constructor(props){
+        super(props)
+        this.state={
+            populares:[],
+            upcoming:[]
+    }
+   
+    }
+    componentDidMount(){
         fetch("https://api.themoviedb.org/3/movie/popular?api_key=75196a6b12119e0621f7373e3de1a94a")
             .then( res => res.json())
             .then(data=> this.setState({
@@ -22,13 +26,17 @@ function Home(props){
              }))
             .catch()
     };
-    return(
-        <div>
-    <Buscador/>
-    {populares.map((peli, idx)=> <Pelis name={peli.results.title} descripcion={peli.result.overview} imagen={peli.results.poster_path}/>)}
-    {upcoming.map((peliup, idx)=> <PelisUp name={peli.results.title} descripcion={peli.result.overview} imagen={peli.results.poster_path}/>)}
+
+    render(){
+        return(
+            <div>
+        <Buscador/>
+        {this.state.populares.map((peli, idx)=> <Pelis name={peli.results.title} descripcion={peli.result.overview} imagen={peli.results.poster_path}/>)}
+        {this.state.upcoming.map((peliup, idx)=> <PelisUp name={peliup.results.title} descripcion={peliup.result.overview} imagen={peliup.results.poster_path}/>)}
+        
+        </div>
+        )
+    }
     
-    </div>
-    )
 }
 export default Home;
