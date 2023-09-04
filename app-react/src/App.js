4ab059e753629cdeb3b67c4298d743e04bec2f95
DelfinaPinasco <pinascod@udesa.edu.ar>
2023-09-04 15:16:38 -0300

    imports en app.js

--- app-react/src/App.js
+++ app-react/src/App.js
@@ -2,6 +2,11 @@ import React from "react";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import {BrowserRouter, Link, Route, Switch} from 'react-router-dom';
+import Detalle from "./screens/Detalle/Detalle";
+import Favoritos from "./screens/Favoritos/Favoritos";
+import Home from "./screens/Home/Home";
+import Popular from "./screens/Popular/Popular";
+import Upcoming from "./screens/Upcoming/Upcoming";
 
 
 function App() {
