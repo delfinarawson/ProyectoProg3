a60316440bfadce487c61b72da20ed8375c246ee
justinaalric <jalricferre@udesa.edu.ar>
2023-09-09 13:50:15 -0300

    classnames

--- app-react/src/screens/Results/Results.js
+++ app-react/src/screens/Results/Results.js
@@ -25,19 +25,19 @@ class Results extends Component{
     render(){
         return(
         <React.Fragment>
-        <section class="contenedor">
+        <section className="contenedor">
 
-        <button role="boton" id="flecha" class="flecha-izquierda"><i class="icon-angle-left"></i></button>
-        <article class="contenedor-peliculas">
-            <h2 class="titulo">Resultados de Peliculas</h2>
-            <div class="galeria">
+        <button role="boton" id="flecha" className="flecha-izquierda"><i className="icon-angle-left"></i></button>
+        <article className="contenedor-peliculas">
+            <h2 className="titulo">Resultados de Peliculas</h2>
+            <div className="galeria">
                 {
                     this.state.resultados.map((resultados, indx)=>
                <Pelis datosPop={resultados}/>)
     }
             </div>
         </article>
-        <button role="boton" id="flecha" class="flecha-derecha"><i class="icon-angle-right"></i></button>
+        <button role="boton" id="flecha" className="flecha-derecha"><i className="icon-angle-right"></i></button>
         </section>
         
         </React.Fragment>
