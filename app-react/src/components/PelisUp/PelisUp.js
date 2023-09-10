359b1daa211b509b549318b6e2143fffa00fe12e
DelfinaPinasco <pinascod@udesa.edu.ar>
2023-09-09 22:42:34 -0300

    funciona boton

--- app-react/src/components/PelisUp/PelisUp.js
+++ app-react/src/components/PelisUp/PelisUp.js
@@ -5,8 +5,11 @@ import {Link} from "react-router-dom";
 class Pelis extends Component{
     constructor(props){
         super(props)
-        this.state={}
+        this.state={ view: false, }
     }
+    visibilidad = () => {
+        this.setState({ view: !this.state.view });
+      };
 
     render(){
         return(
@@ -14,8 +17,12 @@ class Pelis extends Component{
 				<img src={`https://image.tmdb.org/t/p/w500${this.props.datosUp.poster_path}`} alt="pelis"/>
 				<h4 className="titulos-peliculas">{this.props.datosUp.title}</h4>
 				<p className="fechas">{this.props.datosUp.release_date}</p>
-                <button OnClick = {() => this.verMas () } > <p className="fechas">Ver más</p> </button>
+                <button onClick={this.visibilidad}>
+                <p className="fechas">Ver más</p>
+                </button>
+                {this.state.view && (
                 <p className="fechas">{this.props.datosUp.overview}</p>
+                )}
                 <Link to={`/detalle/${this.props.datosUp.id}`} className="detalle">Ver detalle</Link>
         </div>
         )
