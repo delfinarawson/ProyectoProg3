dbd859e6a7640bd3dc78bfcae8c915ee9a947801
DelfinaPinasco <pinascod@udesa.edu.ar>
2023-09-10 19:55:46 -0300

    favs (falta todavia)

--- app-react/src/components/PelisUp/PelisUp.js
+++ app-react/src/components/PelisUp/PelisUp.js
@@ -5,17 +5,48 @@ import {Link} from "react-router-dom";
 class Pelis extends Component{
     constructor(props){
         super(props)
-        this.state={ view: false, }
+        this.state={ 
+            view: false, 
+            textoboton: "Agregar a favoritos"
+        }
     }
     visibilidad = () => {
         this.setState({ view: !this.state.view });
       };
+
+    componentDidMount(){
+        let recuperoStorage = localStorage.getItem ("favoritos");
+        
+        if (recuperoStorage !== null) {
+            let favoritos = JSON.parse (recuperoStorage);
+
+            if (favoritos.includes (this.props.datosUp.id)){
+                this.setState ({
+                    textoBoton: "Quitar de favoritos"
+                })
+            }
+        }
+
+    }
+    
+      agregarYSacarDeFavs (id){
+        let favoritos = [];
+        favoritos.push (id);
+
+        let favoritostoString= JSON.stringify(favoritos);
+        localStorage.setItem ("favoritos", favoritostoString);
+
+        this.setState ({
+            textoBoton: "Quitar de favoritos",
+        })
+    }
 
     render(){
         return(
         <div className = "pelicula"> 
 				<img src={`https://image.tmdb.org/t/p/w500${this.props.datosUp.poster_path}`} alt="pelis"/>
 				<h4 className="titulos-peliculas">{this.props.datosUp.title}</h4>
+                <button onClick={()=> this.agregarYSacarDeFavs(this.props.datosUp.id)} type="button"> {this.state.textoboton} </button>
 				<p className="fechas">{this.props.datosUp.release_date}</p>
                 <button onClick={this.visibilidad}>
                 <p className="fechas">Ver más</p>
