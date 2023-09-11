0aac0a165d472e6c51ef133a487209a8abaeff1a
justinaalric <jalricferre@udesa.edu.ar>
2023-09-11 16:03:21 -0300

    intento de favs

--- app-react/src/components/Favoritos/Favoritos.js
+++ app-react/src/components/Favoritos/Favoritos.js
@@ -1,16 +1,44 @@
 import React from "react";
 import Pelis from "../Pelis/Pelis";
-
-function Favoritos(){
-
+import React, {Component} from React
+class Favoritos extends Component{
+constructor(props){
+    super(props);
+    this.state = {
+        favoritas: []
+    }
+}
+componentDidMount(){
+    let favs = [];
+    let recuperoStoragePelis = localStorage.getItem("favoritos")
+    if(recuperoStoragePelis != null){
+        let pelisArray = JSON.parse(recuperoStoragePelis);
+        favs = pelisArray
+    }
+    favs.map((id)=> {
+        fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=75196a6b12119e0621f7373e3de1a94a`)
+        .then(response => response.json())
+        .then( favs =>{
+            let pelisLista = this.state.favoritas
+            pelisLista.push(favs);
+            this.setState({favoritas: pelisLista})
+        })
+        .catch(error => console.log(error))
+    })
+}
+render(){
     return(
         <React.Fragment>
         <h3 className="seleccion">Selección de favoritos</h3>
         <section className="contenedor">
             <button role="boton" id="flecha-derecha" className="flecha-izquierda"><i className="icon-angle-left"></i></button>
             <article className="contenedor-peliculas">
-                <div className="galeria">
-                    <Pelis/>
+                <div className="galeria">{
+                    this.state.favs.map(
+                        (peliFav, idx) => <Pelis key={peli + idx} datosPop={peliFav}/>
+                    )
+                }
+                    
                 </div>
             </article>
             <button role="boton" id="flecha-derecha" className="flecha-derecha"><i className="icon-angle-right"></i></button>
@@ -19,5 +47,5 @@ function Favoritos(){
         </React.Fragment>
     )
 }
-
+}
 export default Favoritos;
