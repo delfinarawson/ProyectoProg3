5b2b95bf82667c789c090ac894ef17bf5aca8ea3
justinaalric <jalricferre@udesa.edu.ar>
2023-09-11 15:35:24 -0300

    favpritos

--- app-react/src/components/Favoritos/Favoritos.js
+++ app-react/src/components/Favoritos/Favoritos.js
@@ -1,7 +1,22 @@
+import React from "react";
+import Pelis from "../Pelis/Pelis";
+
 function Favoritos(){
 
     return(
-        <h1>Favoritos</h1> //falta completar esto
+        <React.Fragment>
+        <h3 className="seleccion">Selección de favoritos</h3>
+        <section className="contenedor">
+            <button role="boton" id="flecha-derecha" className="flecha-izquierda"><i className="icon-angle-left"></i></button>
+            <article className="contenedor-peliculas">
+                <div className="galeria">
+                    <Pelis/>
+                </div>
+            </article>
+            <button role="boton" id="flecha-derecha" className="flecha-derecha"><i className="icon-angle-right"></i></button>
+        </section>
+
+        </React.Fragment>
     )
 }
 
