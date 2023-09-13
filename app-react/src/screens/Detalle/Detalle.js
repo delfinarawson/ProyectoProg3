b504ee3e5ad35e2d5151480b162e7918b41644db
DelfinaPinasco <pinascod@udesa.edu.ar>
2023-09-13 16:29:18 -0300

    aparece titulo en detalle

--- app-react/src/screens/Detalle/Detalle.js
+++ app-react/src/screens/Detalle/Detalle.js
@@ -29,7 +29,7 @@ class Detalle extends Component{
         <h1 class="titulo"></h1>
         <article class="bloque">
             {
-                <Detail poster_path={this.state.peliculaDet.poster_path} titulo={this.state.peliculaDet.name} fecha={this.state.peliculaDet.release_date} genero={this.state.peliculaDet.genres === undefined ?[]: this.state.peliculaDet.genres} duracion={this.state.peliculaDet.runtime + " Minutos"} calificacion={this.state.peliculaDet.vote_average} />
+                <Detail poster_path={this.state.peliculaDet.poster_path} titulo={this.state.peliculaDet.original_title} fecha={this.state.peliculaDet.release_date} genero={this.state.peliculaDet.genres === undefined ?[]: this.state.peliculaDet.genres} duracion={this.state.peliculaDet.runtime + " Minutos"} calificacion={this.state.peliculaDet.vote_average} />
             }
            
 
